3c534c14d92a31fdf9c78c98ebbd92d1c3221858
Thales <thales.souz@outlook.com>
2022-08-25 20:32:16 -0300

    Projeto Netflix Clone

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
@@ -8,11 +8,11 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path='/home' component={Home} />
-        <Route path='/profile' component={Profile} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={ <Login/> } />
+        <Route path='/home' element={ <Home/> } />
+        <Route path='/profile' element={ <Profile/> } />
+      </Routes>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGear } from '@fortawesome/free-solid-svg-icons'
 import Header from '../components/Header';
-import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { connect, useDispatch } from 'react-redux';
 import { userAction } from '../redux/actions';
 import './Login.css';
 
-class Login extends React.Component {
-  state = {
-    username: '',
-    imgSrc: "https://i.pinimg.com/originals/1a/04/94/1a0494f328556a2f18f27c01a7a2d13b.jpg",
+function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [state, setState] = useState({
+      username: '',
+      imgSrc: "https://i.pinimg.com/originals/1a/04/94/1a0494f328556a2f18f27c01a7a2d13b.jpg",
+     });
+
+  const buttonClick = () => {
+
+    dispatch(userAction(state.username.toUpperCase(), state.imgSrc));
+    navigate("home");
   }
 
-  buttonClick = () => {
-    const { dispatch, history } = this.props;
-    const { username, imgSrc } = this.state;
-    dispatch(userAction(username.toUpperCase(), imgSrc));
-    history.push("/home");
-  }
-
-  imgBtnClick = () => {
+  const imgBtnClick = () => {
     const imgOptions = document.querySelector(".img-options");
     if (imgOptions.style.display === '' || imgOptions.style.display === "none") {
       imgOptions.style.display = "flex";
@@ -28,12 +30,11 @@ class Login extends React.Component {
     }
   }
 
-  changeProfileImage = ({ target }) => {
+  const changeProfileImage = ({ target }) => {
     const imgOptions = document.querySelector(".img-options");
-    const { imgSrc } = this.state;
     const aux = target.src;
-    target.src = imgSrc;
-    this.setState({ imgSrc: aux })
+    target.src = state.imgSrc;
+    setState({ ...state, imgSrc: aux })
     if (imgOptions.style.display === '' || imgOptions.style.display === "none") {
       imgOptions.style.display = "flex";
     } else {
@@ -41,12 +42,10 @@ class Login extends React.Component {
     }
   }
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
+  const handleChange = ({ target: { name, value } }) => {
+    setState({ ...state, [name]: value });
   }
 
-  render() {
-    const { username, imgSrc } = this.state;
     return (
       <main>
         <Header />
@@ -58,8 +57,8 @@ class Login extends React.Component {
               id="input-login"
               type="text"
               name="username"
-              value={username}
-              onChange={this.handleChange}
+              value={state.username}
+              onChange={handleChange}
               placeholder="Insira seu nome..."
             />
           </label>
@@ -67,30 +66,29 @@ class Login extends React.Component {
         <section className="user-img-container">
           <div className="aux-container">
           <img
-            src={ imgSrc }
+            src={ state.imgSrc }
             alt="Foto de perfil do usuario"
             className="user-img"
             width="250px">
           </img>
           <button type='button' className="btn-img"
-            onClick={this.imgBtnClick}>
+            onClick={imgBtnClick}>
             Mudar imagem {' '}
             <FontAwesomeIcon icon={faGear} />
           </button>
           <div className="img-options">
-            <img className="option" src="https://i.pinimg.com/564x/1d/ce/9a/1dce9a824338b9cad4b671ab020cf8cd.jpg" alt="profile" role="button" width="200px" onClick={ this.changeProfileImage }></img>
-            <img className="option" src="https://i.pinimg.com/564x/03/67/85/036785fa8f54085666226a67063bcfa9.jpg" alt="profile" role="button" width="200px" onClick={ this.changeProfileImage }></img>
-            <img className="option" src="https://i.pinimg.com/564x/f2/03/30/f20330ccb1f765c46b3f2fbf0037206e.jpg" alt="profile" role="button" width="200px" onClick={ this.changeProfileImage }></img>
-            <img className="option" src="https://i.pinimg.com/564x/15/8e/34/158e3449e53016326cc3ed5ef6860b25.jpg" alt="profile" role="button" width="200px" onClick={ this.changeProfileImage }></img>
-            <img className="option" src="https://i.pinimg.com/564x/01/d9/82/01d982939e4c55709b18713534a0a25c.jpg" alt="profile" role="button" width="200px" onClick={ this.changeProfileImage }></img>
+            <img className="option" src="https://i.pinimg.com/564x/1d/ce/9a/1dce9a824338b9cad4b671ab020cf8cd.jpg" alt="profile" role="button" width="200px" onClick={ changeProfileImage }></img>
+            <img className="option" src="https://i.pinimg.com/564x/03/67/85/036785fa8f54085666226a67063bcfa9.jpg" alt="profile" role="button" width="200px" onClick={ changeProfileImage }></img>
+            <img className="option" src="https://i.pinimg.com/564x/f2/03/30/f20330ccb1f765c46b3f2fbf0037206e.jpg" alt="profile" role="button" width="200px" onClick={ changeProfileImage }></img>
+            <img className="option" src="https://i.pinimg.com/564x/15/8e/34/158e3449e53016326cc3ed5ef6860b25.jpg" alt="profile" role="button" width="200px" onClick={ changeProfileImage }></img>
+            <img className="option" src="https://i.pinimg.com/564x/01/d9/82/01d982939e4c55709b18713534a0a25c.jpg" alt="profile" role="button" width="200px" onClick={ changeProfileImage }></img>
           </div>
           </div>
         </section>
-        <button type='button' onClick={this.buttonClick} className="btn">Bora assistir!</button>
+        <button type='button' onClick={buttonClick} className="btn">Bora assistir!</button>
       </main>
     );
   }
-}
 
 const mapStateToProps = (store) => ({
   globalState: store.reducer,
